baf5f43569fb598620fb61304fcbecf4efe21e76
mrchrisfrost <androidpatmdev@gmail.com>
2014-01-22 14:08:42 -0500

    improved website

---
+++ aboutme.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <title>About Me</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <div class="header"> About Me
+       
+        </div>
+       
+        <p align="right"> <table>
+            <tr>
+                 <td><font color="white">
+                    <a href="index.php"style="color: red">Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td><font color="white"><a href="aboutme.php"style="color: red">About Me</a></td>
+                <td><font color="white">&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="contactme.php"style="color: red">Contact Me</a></td>
+                <td><font color="white">&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="http://www.facebook.com/chris.frost.3110"style="color: red">Facebook
+                    </a></td>
+                    <td><font color="white">&nbsp;&nbsp;&nbsp;&nbsp;
+                        <a href="http://www.twitter.com/thechrisfrost"style="color: red">Twitter</a>
+                          
+            </tr>
+   </table></p>
+   <div class="content">
+       <img src="me.jpg"/> <img src="james.jpg"height="377px"/> <img src="vaderclaus.jpg"height="377px"/>
+   </div>
+   <div class="main"><font color ="white">
+       Once again welcome. At this point I guess you're wondering a little about me, what makes me tick and why I'm applying to<br>
+       the program, and also what makes me a good candidate. To answer those I guess it's best to start with why I chose those<br>
+       the pictures I did. *** Bonus points if you can tell me the name of the attachment on my rifle.***<p>
+           Well the first one, that was me not too long ago. Running around the mountains of Afghanistan doing things, that no one else<br>
+           wants to do. Even rather I say, shouldn't have to do. But 'eh, it's a life and chose it. Facing challenges is something that has<br>
+           deterred me from doing what I wanted to do, even when creating those challenges just to see if I could overcome them was<br>
+           a self action. The picture next to it is another picture of strength, taken outside a hall at my mother's funeral. Even if times <br>
+           of pure sadness I keep moving forward. Next we have VADER KLAUS, in crowds where everyone is the same, I find a way to <br>stand out.
+           The eagle on my splash page symbolizes how I feel at right now I'm spreading my wings getting ready to take <br>that dive and capture
+           my pray. The background here shows that I'm willing to get down and dirty to do it. <br> And lastly the contact page... <h2>The World is my Objective!</h2>
+           
+       </p>
+       
+        </div>
+        <div class="footer"> 
+          
+        </div>
+        &copy;PATM Technologies 2014
+        
+    </body>
+   
+</html>
---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>Home Page</title>
+        <link rel="stylesheet" href="stylehome.css">
+    </head>
+    <body>
+    <body bgcolor ="black"><h1><font color="white"><font-face="arial"> 
+            <center> Welcome to my Home Page</center></h1></body>
+                <br><p>
+    <center>Please see the about me page to learn more about myself to help
+        you in your decision in selecting me for your program.</center> </p>
+            <table>
+            <tr>
+                <td><font color="white">
+                    <a href="index.php"style="color: red">Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td><font color="white"><a href="aboutme.php"style="color: red">About Me</a></td>
+                <td><font color="white">&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="contactme.php"style="color: red">Contact Me</a></td>
+                <td><font color="white">&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="http://www.facebook.com/chris.frost.3110"style="color: red">Facebook
+                    </a></td>
+                    <td><font color="white">&nbsp;&nbsp;&nbsp;&nbsp;
+                        <a href="http://www.twitter.com/thechrisfrost""style="color: red">Twitter</a>
+                          
+            </tr>
+        </table>
+    
+    
+        
+    
+    
+    
+    
+</html>
